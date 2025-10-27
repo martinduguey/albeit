ed3ba750ad9ad1466ba1352f15a41124622cdb55
Martin <martin.duguey@gmail.com>
2025-10-27 14:09:06 +0100

    [Switch] files structure

---
+++ header/albeit_matrix.hpp
@@ -0,0 +1,18 @@
+class AlbeitMatrix{
+
+    public:
+    
+        //Builder
+        AlbeitMatrix(int nb_rows, int nb_columns);
+        ~AlbeitMatrix() = default;
+
+        //Methods
+        int getNbRows() const;
+        int getNbColumns() const;
+
+    private:
+    
+        int nb_rows_;
+	    int nb_columns_;
+
+};
